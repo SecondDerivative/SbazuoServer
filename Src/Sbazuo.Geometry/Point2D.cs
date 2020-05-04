850c465062778ff8730c1e59033cccb13ad5a69c
Xsaril <ALEX.Sytsev.2014@yandex.ru>
2020-05-04 20:24:44 +0300

    implement Point2D and Vector2D structs

---
+++ Src/Sbazuo.Geometry/Point2D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Math;
+
+namespace Sbazuo.Geometry {
+	public struct Point2D {
+
+		/// <summary>
+		/// gets or sets point X coordinate
+		/// </summary>
+		public double X { get; set; }
+
+		/// <summary>
+		/// gets or sets point Y coordinate
+		/// </summary>
+		public double Y { get; set; }
+
+		/// <summary>
+		/// create new instance of <see cref="Point2D"/>
+		/// </summary>
+		/// <param name="x"> point x coordinate </param>
+		/// <param name="y"> point y coordinate </param>
+		public Point2D(double x, double y) {
+			this.X = x;
+			this.Y = y;
+		}
+
+		/// <summary>
+		/// calculates distance of points
+		/// </summary>
+		public static double Distance(Point2D a, Point2D b) {
+			return Sqrt(Pow(a.X - b.X, 2) + Pow(a.Y - b.Y, 2));
+		}
+
+	}
+}
